d3285b7a30e59169660e34981071ff7e82e6ee49
grohoczkitimi <45313848+grohoczkitimi@users.noreply.github.com>
2018-11-24 16:29:20 +0100

    Update aktualis.tpl.php
    Fix video file path

--- templates/pages/aktualis.tpl.php
+++ templates/pages/aktualis.tpl.php
@@ -1,6 +1,6 @@
 <h3>Kellemes Ünnepeket kívánunk!</h3>
 
 <video width="569" controls>
-    <source src="images/KESZ_karacsony_2015.mp4" type="video/mp4">
+    <source src="../../images/KESZ_karacsony_2015.mp4" type="video/mp4">
 A böngészője sajnos nem támogatja a videó lejátszását.
 </video>
